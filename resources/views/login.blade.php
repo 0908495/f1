2bcdc8f96f183ce083875d7f53522b8dc91a67ac
Admin <arnodejong@outlook.com>
2017-09-14 13:08:25 +0200

    Created login and register page. Added routes.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>Formula Info</title>
+</head>
+
+<body>
+
+<h1>Login using the form below</h1>
+
+</body>
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title>Formula Info</title>
+</head>
+
+<body>
+
+<h1>Register using the form below. If you're an official F1 Team, use the code you've already received in the designated field.</h1>
+
+</body>
+</html>
